9f51b5324ebaa0c84a87b74747f62c6dddc4e0a0
ghjgrc <kahlenoah596@gmail.com>
2025-05-29 17:27:51 -0400

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "Nxcy Palace."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var subtext = "Just a simple Unblocked games Website."; // set the subtext
 
 // more settings in main.css
 
